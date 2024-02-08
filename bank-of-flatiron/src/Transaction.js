91c6a506d0fd653b4d95520a47c2bac9f613d4be
MungaiS <129229232+MungaiS@users.noreply.github.com>
2024-02-08 09:47:55 +0300

    Modified styling

---
+++ bank-of-flatiron/src/Transaction.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Transaction.css";
+
+function Transaction({date, description, category, amount}) {
+  return (
+    <tr>
+      <td>{date}</td>
+      <td>{description}</td>
+      <td>{category}</td>
+      <td>{amount}</td>
+    </tr>
+  );
+}
+
+export default Transaction;
