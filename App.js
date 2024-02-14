e41e0c51de674d7d8305cc3e5ff1ba182087167e
MungaiS <129229232+MungaiS@users.noreply.github.com>
2024-02-14 15:55:58 +0300

    corrected fetch

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import AccountContainer from "./AccountContainer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="ui raised segment">
+      <div className="ui segment violet inverted">
+        <h2>The Royal Bank of Flatiron</h2>
+      </div>
+      <AccountContainer />
+    </div>
+  );
+}
+
+export default App;
